14d614554f45c0ee8ec0a8f6e551f449ddf7b57d
jingzheins <1263078673@qq.com>
2019-12-02 15:54:57 +0800

    第一次提交

---
+++ src/main/java/com/jingzhe/controljvm/ControljvmApplication.java
@@ -0,0 +1,13 @@
+package com.jingzhe.controljvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControljvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControljvmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jingzhe/controljvm/ControljvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingzhe.controljvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControljvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
